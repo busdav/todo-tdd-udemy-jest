2527a1ae3dd31108243eb8d23db3eab81999c8f0
David Buser <busdav@outlook.com>
2019-10-22 10:03:04 +0100

    Complete unit tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,6 @@
 const TodoModel = require("../model/todo.model");
 
 exports.createTodo = (req, res, next) => {
-  TodoModel.create(req.body);
+  const createdModel = TodoModel.create(req.body);
+  res.status(201).json(createdModel);
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -3,20 +3,36 @@ const TodoModel = require("../../model/todo.model");
 const httpMocks = require("node-mocks-http");
 const newTodo = require("../mock-data/new-todo.json");
 
-TodoModel.create = jest.fn();
+TodoModel.create = jest.fn(); // Mock, because we are assuming that mongoose is working properly.
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null; 
+})
 
 describe("TodoController.createTodo", () => {
+  
+beforeEach(() => {
+  req.body = newTodo;
+})
+
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
-
   it("should call TodoModel.create", () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null; 
-    req.body = newTodo;
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
-  })
+  });
+  it("should return 201 response code", () => {
+    TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+  it("should return json body in response", () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    TodoController.createTodo(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newTodo);
+  });
 });
