6c0035a286a74ac973a35a8c6143b09d63ae07ef
David Buser <busdav@outlook.com>
2019-10-26 10:34:19 +0100

    Make async test calls; add server.js

--- mongodb/mongodeb.connect.js
+++ mongodb/mongodeb.connect.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
-      "mongodb+srv://SuperTestUser:mypassword@cluster0-miqjw.mongodb.net/test?retryWrites=true&w=majority"
+      "mongodb+srv://SuperTestUser:mypassword@cluster0-miqjw.mongodb.net/test?retryWrites=true&w=majority",
+      { useNewUrlParser: true, 
+        useUnifiedTopology: true }
       );
   } catch (err) {
     console.error("Error connecting to mongodb");
