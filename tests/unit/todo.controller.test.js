d7ae4df8a0b2ce5652563bd334caa9d1a62553ec
David Buser <busdav@outlook.com>
2019-10-27 10:00:37 +0000

    Add error handling

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -9,7 +9,7 @@ let req, res, next;
 beforeEach(() => {
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
-  next = null; 
+  next = jest.fn(); 
 })
 
 describe("TodoController.createTodo", () => {
@@ -35,4 +35,11 @@ beforeEach(() => {
     await TodoController.createTodo(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newTodo);
   });
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Done property missing" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    TodoModel.create.mockReturnValue(rejectedPromise);
+    await TodoController.createTodo(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
+  })
 });
