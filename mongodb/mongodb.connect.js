da37ac16d1095371f45eb8b560e269428a2980f8
David Buser <busdav@outlook.com>
2019-11-05 13:02:45 +0000

    Separate test db

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -2,11 +2,19 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://SuperTestUser:mypassword@cluster0-miqjw.mongodb.net/test?retryWrites=true&w=majority",
-      { useNewUrlParser: true, 
-        useUnifiedTopology: true }
+    if (process.env.NODE_ENV == "test") {
+      await mongoose.connect(
+        "mongodb+srv://SuperTestUser:mypassword@cluster0-miqjw.mongodb.net/test?retryWrites=true&w=majority",
+        { useNewUrlParser: true, 
+          useUnifiedTopology: true }
       );
+    } else {
+      await mongoose.connect(
+        "mongodb+srv://SuperTestUser:mypassword@cluster0-miqjw.mongodb.net/dev?retryWrites=true&w=majority",
+        { useNewUrlParser: true, 
+          useUnifiedTopology: true }
+      );
+    }
   } catch (err) {
     console.error("Error connecting to mongodb");
     console.error(err);
